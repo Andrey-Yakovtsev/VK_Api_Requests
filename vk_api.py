e0b7ae8bd3bd241952e5d956497dbb4a37b92da5
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-06-02 14:12:35 +0300

    Не идет функция по списку друзей

--- vk_api.py
+++ vk_api.py
@@ -25,28 +25,28 @@ class User:
             'access_token': token,
             'v': 5.89,
         }
-    def get_status(self):
-        params = self.get_params()
-        URL = 'https://api.vk.com/method/status.get'
-        response = requests.get(URL, params)
-        return response.json()['response']['text']
+    # def get_status(self):
+    #     params = self.get_params()
+    #     URL = 'https://api.vk.com/method/status.get'
+    #     response = requests.get(URL, params)
+    #     return response.json()['response']['text']
+    #
+    # def set_status(self, text):
+    #     params = self.get_params()
+    #     params['text'] = text
+    #     URL = 'https://api.vk.com/method/status.set'
+    #     response = requests.get(URL, params)
+    #     return response.json()['response']
 
-    def set_status(self, text):
-        params = self.get_params()
-        params['text'] = text
-        URL = 'https://api.vk.com/method/status.set'
-        response = requests.get(URL, params)
-        return response.json()['response']
-
-    def get_wall_messages(self, counter):
-        params = self.get_params()
-        params['domain'] = self.domain
-        params['owner_id'] = self.owner_id
-        params['count'] = counter
-        URL = 'https://api.vk.com/method/wall.get'
-        response = requests.get(URL, params)
-        for item in response.json()['response']['items']:
-             print(item['text'])
+    # def get_wall_messages(self, counter):
+    #     params = self.get_params()
+    #     params['domain'] = self.domain
+    #     params['owner_id'] = self.owner_id
+    #     params['count'] = counter
+    #     URL = 'https://api.vk.com/method/wall.get'
+    #     response = requests.get(URL, params)
+    #     for item in response.json()['response']['items']:
+    #          print(item['text'])
 
     def get_friends(self, count):
         params = self.get_params()
@@ -55,21 +55,20 @@ class User:
         params['fields'] = 'nickname', 'domain'
         URL = 'https://api.vk.com/method/friends.get'
         response = requests.get(URL, params)
-        i=0
+        friends_list = []
         for item in response.json()['response']['items']:
-            i+=1
-            print (i, '==>', item['first_name'], item['last_name'], item['domain'], item['id'])
-
-    def get_mutual_friends(self, friend1, friend2):
-        params = self.get_params()
-        # user1 = self.owner_id
-        # user2 = self.owner_id
-        params['source_uid'] = self.owner_id     # с каким пользователем сравниваем
-        params['target_uids'] = [friend1.owner_id, friend2.owner_id]     # какого пользователя сравниваем
-        URL = 'https://api.vk.com/method/friends.getMutual'
-        response = requests.get(URL, params)
-        for item in response.json()['response']:
-            return item['common_friends'], len(item['common_friends'])
+            friends_list.append(item['id'])
+        return friends_list
+    # def get_mutual_friends(self, friend1, friend2):
+    #     params = self.get_params()
+    #     # user1 = self.owner_id
+    #     # user2 = self.owner_id
+    #     params['source_uid'] = self.owner_id     # с каким пользователем сравниваем
+    #     params['target_uids'] = [friend1.owner_id, friend2.owner_id]     # какого пользователя сравниваем
+    #     URL = 'https://api.vk.com/method/friends.getMutual'
+    #     response = requests.get(URL, params)
+    #     for item in response.json()['response']:
+    #         return item['common_friends'], len(item['common_friends'])
 
     def get_user_groups(self):
         params = self.get_params()
@@ -80,14 +79,27 @@ class User:
         print(response.json()['response'])
 
 
+    def user_friends_groups_lookup(self, groupid):
+        params = self.get_params()
+        params['user_ids'] = ['3730376', '268721993']   # self.get_friends(100)
+        params['extended'] = 1
+        params['group_id'] = groupid
+        URL = 'https://api.vk.com/method/groups.isMember'
+        response = requests.get(URL, params)
+        print(response.json())
+
+
+
 
 Andrey = User(token, 'https://vk.com/ayakovtsev', 3293131)
 Genka = User(token, 'https://vk.com/id3730376', 3730376)
 Sofya = User(token, 'https://vk.com/s.yakovtseva', 268721993)
 Yana = User(token, 'https://vk.com/id4867400', 4867400)
 
+
+pprint(Andrey.user_friends_groups_lookup(90329578))
 pprint(Andrey.get_user_groups())
-pprint(Andrey.get_friends(2500))
+# pprint(Andrey.get_friends(500))
 
 
 
